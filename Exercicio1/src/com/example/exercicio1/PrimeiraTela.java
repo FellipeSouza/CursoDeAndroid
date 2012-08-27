1a709289087a3336c8ae27b96dc89bfbd7d96027
FellipeSouza <fellipesouza@hotmail.com.br>
2012-08-27 14:06:41 -0300

    Update Exercicio1/src/com/example/exercicio1/PrimeiraTela.java

--- Exercicio1/src/com/example/exercicio1/PrimeiraTela.java
+++ Exercicio1/src/com/example/exercicio1/PrimeiraTela.java
@@ -1,10 +1,11 @@
 /*
- * 1) Tabela din�mica
-	a) Construa um programa com um campo de texto e um bot�o. 
-	Esse bot�o deve adicionar o texto inserido em uma tabela 
+ * 1) Tabela dinamica
+	a) Construa um programa com um campo de texto e um botao. 
+	Esse botao deve adicionar o texto inserido em uma tabela 
 	(criar uma linha e adicionar o texto nessa linha).
+	
 	b) Altere o programa para que ele adicione o texto do campo
-	em uma nova linha da tabela toda vez que o bot�o seja clicado.
+	em uma nova linha da tabela toda vez que o botao seja clicado.
  */
 
 package com.example.exercicio1;
@@ -23,10 +24,10 @@ import android.widget.TableRow.LayoutParams;
 
 public class PrimeiraTela extends Activity {
 
-	/* Criando o ArrayList que guardar� os textos digitados */
+    /* Criando o ArrayList que guardara os textos digitados */
     private static ArrayList<String> textos = new ArrayList<String>();
     
-    /* String que passar� os dados do campo pro ArrayList */
+    /* String que passara os dados do campo pro ArrayList */
     String aux;
 	
     @Override
@@ -34,31 +35,31 @@ public class PrimeiraTela extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.primeira_tela);
 
-        /* Referenciando Campo de Texto, Bot�o e Tabela do XML */
+        /* Referenciando Campo de Texto, Botao e Tabela do XML */
         final EditText campoTexto = (EditText) findViewById(R.id.campoTexto);
         Button botao = (Button) findViewById(R.id.botao);
         final TableLayout tabela = (TableLayout) findViewById(R.id.tabela);
 		
-        /* Definindo os par�metros da tabela em uma vari�vel */
+        /* Definindo os par�metros da tabela em uma variavel */
 	final TableRow.LayoutParams parametroTabela = new TableRow.LayoutParams(LayoutParams.FILL_PARENT, 
 				LayoutParams.WRAP_CONTENT);
 
-		/* Definindo as fun��es do clique do bot�o */
+		/* Definindo as funcoes do clique do botao */
         botao.setOnClickListener(new View.OnClickListener() {
 			
 		@Override
 		public void onClick(View v) {
-			/* aux recebe o conte�do do campo de texto */				
+			/* aux recebe o conteudo do campo de texto */				
 			aux = campoTexto.getText().toString();
 			
 			if (aux != null) {
-				/* ArrayList recebe o conte�do do aux */
+				/* ArrayList recebe o conteudo do aux */
 		        	textos.add(aux);
 		        	
-		        	/* Deleta o conte�do do campo de texto */
+		        	/* Deleta o conteudo do campo de texto */
 		        	campoTexto.setText("");
 		        	
-		        	/* La�o de repeti��o que vai inserir os dados na tabela */
+		        	/* Laco de repeticao que vai inserir os dados na tabela */
 		        	for (int i = 0; i < textos.size(); i++) {
 		        		
 		        		/* Criando linha e campo da tabela */
